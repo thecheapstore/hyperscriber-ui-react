bc2a3ec1dce5a4b339694960a3e4840db1c32dd2
thecheapstore <jackwinchester943@gmail.com>
2026-01-04 18:46:50 -0500

    modified:   src/components/our-team/page.tsx

--- src/components/our-team/page.tsx
+++ src/components/our-team/page.tsx
@@ -1,4 +1,17 @@
 import TeamHero from "@/components/our-team/TeamHero";
 import TeamStrengths from "@/components/our-team/TeamStrengths";
-import TeamMembers from "@/components/our-team/TeamMembers"; // you can add later
+import TeamMembers from "@/components/our-team/TeamMembers";
 import TeamValues from "@/components/our-team/TeamValues";
+
+const OurTeamPage = () => {
+  return (
+    <>
+      <TeamHero />
+      <TeamStrengths />
+      <TeamMembers />
+      <TeamValues />
+    </>
+  );
+};
+
+export default OurTeamPage;
