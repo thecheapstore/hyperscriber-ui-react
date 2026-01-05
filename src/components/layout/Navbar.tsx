e78be87c841924a43853a03f1c20a5394736b917
thecheapstore <jackwinchester943@gmail.com>
2026-01-05 16:19:19 -0500

    logo change

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -43,7 +43,7 @@ const Navbar = () => {
           className="flex items-center z-30 gap-2"
           onClick={() => window.scrollTo(0, 0)}
         >
-          <img src="https://i.ibb.co/ksMhQrst/94e69e74-31c6-4907-b7da-719956c4355f.png" alt="Calibre Associates Logo" className="h-20 w-10 object-contain" />
+          <img src="https://i.ibb.co/4ZVn3yZs/a8cb39ee-8dbf-47b5-b9fa-010b3a870e14-removebg-preview.png" alt="Calibre Associates Logo" className="h-20 w-10 object-contain" />
           <span className="text-xl md:text-2xl font-bold text-black">
             Clibre Associates
           </span>
